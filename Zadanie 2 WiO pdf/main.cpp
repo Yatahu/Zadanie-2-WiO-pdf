ea287fa4845ba6b6c19f39dd5e8d8806fa89f47f
Wojtek <wojtekszyjka@outlook.com>
2020-11-13 15:42:39 +0100

    Initial Commit

---
+++ Zadanie 2 WiO pdf/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Zadanie 2 WiO pdf
+//
+//  Created by Wojciech Szyjka on 13/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
