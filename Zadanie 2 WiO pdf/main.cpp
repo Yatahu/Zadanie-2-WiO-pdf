d1c6a3e345f8600ff801ecf0132008e6590bbda5
Wojtek <wojtekszyjka@outlook.com>
2020-11-20 21:58:08 +0100

    +

--- Zadanie 2 WiO pdf/main.cpp
+++ Zadanie 2 WiO pdf/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float a ;
+float b ;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+   
+    cout << "Podaj liczbę a " ;
+    cin >> a ;
+    cout << endl ;
+    cout << "Podaj liczbę b " ;
+    cin >> b ;
+    cout << endl ;
+    
+    if ( (a >= 10 && a < 100 ) || (b >=10 && b < 100) )
+        cout <<  "a lub b 2 cyfrowa : true"<< endl ;
+    
+    else
+        cout << "a lub b 2 cyfrowa : false" << endl ;
+    
+    return 0 ;
+        
 }
